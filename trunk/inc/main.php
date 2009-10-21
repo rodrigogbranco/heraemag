0c8b3b9449780b41866249d71d219546756cfc60
rodrigo.g.branco <rodrigo.g.branco@d93dc3e8-70b9-11de-90d8-1fdda9445408>
2009-10-21 15:48:44 +0000

    com scripts começados

--- trunk/inc/main.php
+++ trunk/inc/main.php
@@ -16,6 +16,10 @@ require_once ("inc/common.php"); // Some core libraries
 require_once('inc/resumen.php'); // Class to build the summary of results
 cleanAll();
 
+var_dump($_REQUEST);
+var_dump($_POST);
+var_dump($_GET);
+
 /*RGB begin*/
 if(isset($_POST['choose']))
 {
